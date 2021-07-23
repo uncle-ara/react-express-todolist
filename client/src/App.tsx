810d9a1d02aa676b41f2d293eb014db82cbd4729
uncle-ara <m.ozensel@yandex.ru>
2021-07-24 00:10:37 +0300

    Added Filters and Todo components; Applied design to them

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import "./App.css";
+import React from "react"
+import Filters from "./components/Filters/Filters"
+import Todo from "./components/Todo/Todo"
 
-function App() {
-  return <div className="App"></div>;
+const App = () => {
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoList</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="button" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <Filters />
+      <Todo />
+    </div>
+  )
 }
 
-export default App;
+export default App
